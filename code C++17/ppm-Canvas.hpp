ba2989cd6b2757482f5ec6f915c8b7122c52a63d
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-03-15 16:38:02 +0100

    Update ppm-Canvas.hpp

--- code C++17/ppm-Canvas.hpp
+++ code C++17/ppm-Canvas.hpp
@@ -4,6 +4,7 @@
 #include "ppm-Color.hpp"
 
 #include <string>
+#include <optional>
 #include <utility>
 
 namespace ppm {
@@ -11,6 +12,7 @@ namespace ppm {
 
     using   cppu::Matrix_, cppu::in_;
     using   std::string, std::to_string,        // <string>
+            std::optional,                      // <optional>
             std::pair;                          // <utility>
 
     class Canvas
@@ -27,17 +29,12 @@ namespace ppm {
         auto width() const -> int           { return m_pixels.width(); }
         auto height() const -> int          { return m_pixels.height(); }
 
-        void set_pixel( const int x, const int y, in_<Color> color )
+        void set_pixel( const int x, const int y, in_<optional<Color>> color = {} )
         {
-            m_pixels( x, y ) = color;
+            m_pixels( x, y ) = color.value_or( m_drawing_color );
         }
 
-        void set_pixel( const int x, const int y )
-        {
-            set_pixel( x, y, m_drawing_color );
-        }
-
-        void draw_horizontal_at( const int y, const int x1, const int x2, in_<Color> color )
+        void draw_horizontal_at( const int y, const int x1, const int x2, in_<optional<Color>> color = {} )
         {
             if( x1 > x2 ) {
                 draw_horizontal_at( y, x2, x1, color );
@@ -46,23 +43,13 @@ namespace ppm {
             }
         }
         
-        void draw_horizontal_at( const int y, const int x1, const int x2 )
-        {
-            draw_horizontal_at( y, x1, x2, m_drawing_color );
-        }
-
-        void draw_vertical_at( const int x, const int y1, const int y2, in_<Color> color )
+        void draw_vertical_at( const int x, const int y1, const int y2, in_<optional<Color>> color = {} )
         {
             if( y1 > y2 ) {
                 draw_vertical_at( x, y2, y1, color );
             } else {
                 for( int y = y1; y <= y2; ++y ) { set_pixel( x, y, color ); }
             }
-        }
-
-        void draw_vertical_at( const int x, const int y1, const int y2 )
-        {
-            draw_vertical_at( x, y1, y2, m_drawing_color );
         }
 
         template< class Func >
